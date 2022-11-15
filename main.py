5727431276ced0039c39d72aadcc6eaafd2d2b67
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-15 14:28:43 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+from random import seed
+from random import randint
+import time
+import random
+import string
+import requests
+print('''
+\033[36m 
+  _____     _____
+ /____/|   /____/|_______________________________
+|    | |  |    |/________/___________/_________\\ \\
+|    | |  |    |   ___   |     __    |    ___   \\_\\
+|    | /\ |    |  |   |  |    | |\\___|   |   |  | |
+|    |/  \|    |  |___|  |    | |    |   |___|  |_|
+ \             /         |    | |    |          / /
+\033[37m  \___________/__________|____|_|    |_________/_/
+   
+               tiktok jahsehrare
+               insta: spookyle4n\033[37m
+''')
+e = input("[?] Your schoology email?: ")
+d = input("[?] Your schoology password? (bruteforce attack wont work without it): ")
+print("attempting to log into schoology with...\033 " + e)
+time.sleep(4)
+print("\033[32msuccessfully logged in as " + e + " [\u2713]")
+
+c = input("\033[37m[?] Victims schoology email?: ")
+b = int(input("[?] How many passwords would you like to try?: "))
+a = input("[?] Start Attack? [y/n]: ")
+if a == 'y':
+    print("\033[31mStarting bruteforce on " + c)
+    time.sleep(2)
+    for i in range(b):
+        randomnumber = chr(random.randint(ord('0'), ord('9')))
+        randomnumber2 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber3 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber4 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber5 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber6 = chr(random.randint(ord('0'), ord('9')))
+        randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+        randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+        if randomnumber + randomnumber2 + randomnumber3 + randomLowerLetter == '923u':
+            print("\033[32m\n\tsuccess! [\u2713] ")
+            time.sleep(100000000)
+            
+        print("\033[31m[+] attempting... " + randomnumber + randomnumber2 + randomnumber3 + randomLowerLetter, flush=True)
+        time.sleep(0.000001)
